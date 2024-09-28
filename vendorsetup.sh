22ef44a22e38267a198e695a80a6b42a050b3b53
Thiago <diasjuremathiago@gmail.com>
2024-09-28 12:58:47 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 deviceDir=$(gettop)/device/motorola/odessa
 
 # clone motoCam
-git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_logan.git -b eleven-arm64 packages/apps/MotCamera2
+git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_whitney.git -b eleven-arm64 packages/apps/MotCamera2
 
 git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_overlay.git -b ten packages/apps/MotCamera2-overlay
 
