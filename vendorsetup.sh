40584f40364a2247dba80aea4df66374783cf227
Thiago <diasjuremathiago@gmail.com>
2024-09-28 10:42:09 -0300

    attempt to add motocam2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,11 @@
 deviceDir=$(gettop)/device/motorola/odessa
 
+# clone motoCam
+git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_logan.git -b eleven-arm64 packages/apps/MotCamera2
+
+git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_overlay.git -b ten packages/apps/MotCamera2-overlay
+
+git clone https://gitlab.com/NemesisDevelopers/motorola/motorola_motosignatureapp.git -b eleven packages/apps/MotoSignatureApp
+
 # apply patches
 ${deviceDir}/applyPatches.sh ${deviceDir}/patches
