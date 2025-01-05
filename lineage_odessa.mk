2a269b35443aaa1c0ae1ecae2eef990ef59b147f
thz22 <thjr5965@gmail.com>
2025-01-05 10:11:38 -0300

    Update lineage_odessa.mk

--- lineage_odessa.mk
+++ lineage_odessa.mk
@@ -43,8 +43,9 @@ PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # Build info
+BUILD_FINGERPRINT := "motorola/odessa_retail/odessa:11/RPAS31.Q2-59-17-4-5-5/af8e3:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="odessa_retail-user 11 RPAS31.Q2-59-17-4-5-5 af8e3 release-keys" \
-    BuildFingerprint=motorola/odessa_retail/odessa:11/RPAS31.Q2-59-17-4-5-5/af8e3:user/release-keys    
+    PRODUCT_NAME=odessa_retail \
+    PRIVATE_BUILD_DESC="odessa_retail-user 11 RPAS31.Q2-59-17-4-5-5 af8e3 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
