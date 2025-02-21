2e1d4def6f558857afa919c807b7c254672bda8d
thz22 <thjr5965@gmail.com>
2025-02-21 09:48:29 -0300

    aospa

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,6 @@ deviceDir=$(gettop)/device/motorola/odessa
 
 # apply patches
 ${deviceDir}/applyPatches.sh ${deviceDir}/patches
+
+# Aospa Gapps
+git clone https://gitlab.com/ThankYouMario/android_vendor_google_gms.git --single-branch --depth=1 vendor/google/gms 
