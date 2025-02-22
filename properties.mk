5b06856124891b782cfe826d3fcfa2b4dafa9466
thz22 <thjr5965@gmail.com>
2025-02-22 15:50:25 -0300

    Update properties.mk

--- properties.mk
+++ properties.mk
@@ -22,7 +22,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # LOS
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.lineage.maintainer=dias.thiago
+    ro.lineage.maintainer=dias.thiago__
 
 # SIM
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
